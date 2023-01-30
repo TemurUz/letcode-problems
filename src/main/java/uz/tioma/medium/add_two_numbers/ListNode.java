62596974fd212dbe8320c005c1d2a5c071e457c3
TemurUz <temirmaliknomozov@gmail.com>
2023-01-30 17:11:58 +0500

    add hard and medium problems

---
+++ src/main/java/uz/tioma/medium/add_two_numbers/ListNode.java
@@ -0,0 +1,13 @@
+package uz.tioma.medium.add_two_numbers;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode() {
+    }
+
+    ListNode(int val) { this.val = val; }
+
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
---
+++ src/main/java/uz/tioma/medium/length_of_longest_substring/LengthOfLongestSubstring.java
@@ -0,0 +1,22 @@
+package uz.tioma.medium.length_of_longest_substring;
+
+public class LengthOfLongestSubstring {
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+        System.out.println(lengthOfLongestSubstring(s));
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        int max = 0;
+        char [] h = s.toCharArray();
+        StringBuilder g = new StringBuilder();
+        for (int i = 0; i < h.length - 1; i++) {
+            for (int j = 0; j < i; j++) {
+                if (!(h[i] == h[j])){
+                    g.append(h[i]);
+                }
+            }
+        }
+        return max;
+    }
+}
