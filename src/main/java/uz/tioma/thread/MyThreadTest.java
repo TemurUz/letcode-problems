15c624d4351437df9e4ee5e57518265d9ffa9b20
TemurUz <temirmaliknomozov@gmail.com>
2023-01-31 10:26:05 +0500

    Thread based multitasking added

---
+++ src/main/java/uz/tioma/thread/MyThreadTest.java
@@ -0,0 +1,10 @@
+package uz.tioma.thread;
+
+public class MyThreadTest extends Thread{
+    @Override
+    public void run() {
+        for (int i = 0; i < 12; i++) {
+            System.out.println("MyThreadTest is worked ..........");
+        }
+    }
+}
