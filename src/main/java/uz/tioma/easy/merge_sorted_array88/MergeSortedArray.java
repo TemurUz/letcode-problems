2e53f8f8801468015560650a9af6f73961f718f5
TemurUz <temirmaliknomozov@gmail.com>
2023-02-23 17:34:56 +0500

    MergeSortedArray

--- src/main/java/uz/tioma/easy/merge_sorted_array88/MergeSortedArray.java
+++ src/main/java/uz/tioma/easy/merge_sorted_array88/MergeSortedArray.java
@@ -10,9 +10,16 @@ public class MergeSortedArray {
     }
 
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] nums3 = new int[m];
-        for (int i = 0; i < m; i++) {
-            nums3[i] = nums1[i];
+        int i=m-1;
+        int j=n-1;
+        int k=nums1.length-1;
+
+        while(j>=0){
+            if(i>=0 && nums1[i]>nums2[j]){
+                nums1[k--]=nums1[i--];
+            }else{
+                nums1[k--] = nums2[j--];
+            }
         }
     }
 
