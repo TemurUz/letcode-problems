762a743fe8155b5d7a5da8499f793656a54babbf
TemurMalik <nomozovtemurmalik@gmail.com>
2023-05-16 23:13:22 +0500

    ch

--- src/main/java/uz/tioma/search/BinarySearch.java
+++ src/main/java/uz/tioma/search/BinarySearch.java
@@ -5,8 +5,8 @@ import java.util.Date;
 public class BinarySearch {
     public static void main(String[] args) {
 
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 115, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,
-                28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,49,60,61,62};
+
+        int[] arr = getArray();;
         int x = 60;
         int result = binarySearch(arr, x);
         System.out.println("2 - " + new Date().getTime());
@@ -15,6 +15,13 @@ public class BinarySearch {
         else
             System.out.println("Element found at "
                     + arr[result]);
+    }
+
+    static int [] getArray() {
+        return new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 115,
+                16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+                32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
+                48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 49, 60, 61, 62};
     }
 
     public static int binarySearch(int[] arr, int key){
