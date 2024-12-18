4d251360cfcba307c9c0848523e546eaac39ef4d
Temurmalik Namozov <temurmalik.namozov@davrbank.uz>
2024-12-18 15:55:39 +0500

    L1475 done L2182 todo

---
+++ src/main/java/letcode/L2182/Solution.java
@@ -0,0 +1,17 @@
+package letcode.L2182;
+
+public class Solution {
+
+    //todo
+    public String repeatLimitedString(String s, int repeatLimit) {
+        char[] c = new char[s.length()];
+        return "";
+    }
+
+    public static void main(String[] args) {
+        String s = "cczazcc";
+        int repeatLimit = 3;
+        Solution solution = new Solution();
+        System.out.println(solution.repeatLimitedString(s, repeatLimit));
+    }
+}
--- src/main/java/letcode/L643/Solution.java
+++ src/main/java/letcode/L643/Solution.java
@@ -2,25 +2,26 @@ package letcode.L643;
 
 public class Solution {
     public double findMaxAverage(int[] nums, int k) {
-        double res = 0.00000;
+        int sum = 0;
 
+        for (int i = 0; i < k; i++)
+            sum += nums[i];
 
-
-        for (int i = 0; i < k; i ++) {
-            if(k >  nums.length) {
-                i = 0;
-            }
-
-            res += Math.max(nums[i], nums[i + 1]);
-            k--;
+        int maxSum = sum;
+        int len = nums.length;
+        for (int i = k; i < len; i++) {
+            sum += nums[i] - nums[i - k];
+            maxSum = Math.max(maxSum, sum);
         }
-        return res/4;
+        return (double) maxSum / k;
     }
 
     public static void main(String[] args) {
-        int [] nums = {1,12,-5,-6,50,3};
+        int[] nums = {1,12,-5,-6,50,3};
         int k = 4;
         Solution solution = new Solution();
         System.out.println(solution.findMaxAverage(nums, k));
     }
+
+    // l=6 k=4
 }
